fa9d3dd4dc3288a2c721d6af1d3f632fbe43fead
Thomas-Garayt <thomas.garayt@gmail.com>
2017-04-05 17:45:08 +0200

    Preparing pokemon factory

--- src/main/java/fr/univavignon/pokedex/imp/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/imp/PokemonFactory.java
@@ -7,7 +7,14 @@ public class PokemonFactory implements IPokemonFactory {
 	@Override
 	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
 		// TODO Auto-generated method stub
+
 		return null;
+		
+		// Name, Attack, Defense and Stamina has to be get on webservice
+		
+		// IV has to be get using a web calculator
+
+		// Pokemon pokemon = new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv/);	
 	}
 	
 }
