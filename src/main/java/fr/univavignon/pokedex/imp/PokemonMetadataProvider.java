0d3161a1521f7c6f3a54711cfb64ba4d97e8e0d5
Thomas-Garayt <thomas.garayt@gmail.com>
2017-05-03 17:36:07 +0200

    Adding Gson to pom.xml

--- src/main/java/fr/univavignon/pokedex/imp/PokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/imp/PokemonMetadataProvider.java
@@ -1,13 +1,149 @@
 package fr.univavignon.pokedex.imp;
 
-import fr.univavignon.pokedex.api.*;
+import com.google.gson.Gson;
+import fr.univavignon.pokedex.api.IPokemonMetadataProvider;
+import fr.univavignon.pokedex.api.PokedexException;
+import fr.univavignon.pokedex.api.PokemonMetadata;
+import fr.univavignon.pokedex.imp.IGSerializer;
 
-public class PokemonMetadataProvider implements IPokemonMetadataProvider {
+import java.io.*;
+import java.net.URL;
 
-	@Override
-	public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+public class PokemonMetadataProvider implements IPokemonMetadataProvider, IGSerializer {
+
+    private String API = "http://hoomies.fr/pokemeta/?id=";
+
+    //private String rootPath;
+
+    private String path;
+
+    public PokemonMetadataProvider() {
+        //this.rootPath = System.getProperty("user.home") + "/";
+        this.path = ".pokedex42/data/pokemons/";
+        this.initPath(path);
+    }
+
+    @Override
+    public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
+
+        if (index <= 0 || index > 150) {
+            throw new PokedexException("Id is not valid !");
+        }
+
+        String link = API + index;
+
+        PokemonMetadata metadata = null;
+
+        try {
+
+            if (checkFile(path, Integer.toString(index))) {
+                System.out.println("Loading metadata: " + index);
+                metadata = (PokemonMetadata) this.loadData(Integer.toString(index));
+            } else {
+                System.out.println("Downloading metadata: " + index);
+                String content = this.curl(link);
+                metadata = this.json2PokemonMetadata(content);
+                this.saveData(metadata);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return metadata;
+    }
+
+
+    /**
+     * Get metadata infos into json string from API
+     *
+     * @param link
+     * @return
+     * @throws IOException
+     */
+    private String curl(String link) throws IOException {
+
+        URL url = new URL(link);
+
+        String content = "";
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                content += line;
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+
+        return content;
+    }
+
+
+    /**
+     * Create PokemonMetadata from Json
+     *
+     * @param content
+     * @return pokemonMetadata
+     */
+    private PokemonMetadata json2PokemonMetadata(String content) {
+
+        Gson g = new Gson();
+
+        PokemonMetadata pokemonMetadata = g.fromJson(content, PokemonMetadata.class);
+
+        return pokemonMetadata;
+    }
+
+    @Override
+    public void saveData(Object object) throws IOException, PokedexException {
+
+        PokemonMetadata pokemonMetadata = (PokemonMetadata) object;
+
+        if (pokemonMetadata == null) {
+            throw new PokedexException("Couldn't save empty metadata !");
+        }
+
+        if (path == null) {
+            throw new PokedexException("Metadata path is not defined !");
+        }
+
+        String filename = this.getFileName(path, Integer.toString(pokemonMetadata.getIndex()));
+
+
+        this.persistData(filename, pokemonMetadata);
+    }
+
+    @Override
+    public Object loadData(String name) throws FileNotFoundException, PokedexException {
+
+        if (path == null) {
+            throw new PokedexException("Metadata path is not defined !");
+        }
+
+        PokemonMetadata pokemonMetadata = null;
+
+        String filename = this.getFileName(path, name);
+
+        try (Reader reader = new FileReader(filename)) {
+
+            Gson gson = new Gson();
+
+            pokemonMetadata = gson.fromJson(reader, PokemonMetadata.class);
+
+            if (pokemonMetadata == null) {
+                throw new PokedexException("Error loading Metadata !");
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+
+        return pokemonMetadata;
+}
 
 }
--- src/main/java/fr/univavignon/pokedex/imp/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/imp/PokemonTrainerFactory.java
@@ -1,16 +1,102 @@
 package fr.univavignon.pokedex.imp;
 
+import java.io.*;
+
+import com.google.gson.Gson;
+
 import fr.univavignon.pokedex.api.IPokedexFactory;
 import fr.univavignon.pokedex.api.IPokemonTrainerFactory;
+import fr.univavignon.pokedex.api.PokedexException;
 import fr.univavignon.pokedex.api.PokemonTrainer;
 import fr.univavignon.pokedex.api.Team;
 
-public class PokemonTrainerFactory implements IPokemonTrainerFactory {
+public class PokemonTrainerFactory implements IPokemonTrainerFactory, IGSerializer {
 
-	@Override
-	public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private String path;
 
+    public PokemonTrainerFactory() {
+        this.path = ".pokedex42/data/trainers/";
+        this.initPath(path);
+    }
+
+
+    @Override
+    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
+
+        PokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
+
+        PokemonFactory pokemonFactory = new PokemonFactory();
+
+        Pokedex pokedex = (Pokedex) pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+
+        PokemonTrainer pokemonTrainer = null;
+
+        try {
+
+            if (checkFile(path, name)) {
+                System.out.println("Loading " + name + "...");
+                pokemonTrainer = (PokemonTrainer) this.loadData(name);
+            } else {
+                System.out.println("Saving " + name + "...");
+                pokemonTrainer = new PokemonTrainer(name, team, pokedex);
+                this.saveData(pokemonTrainer);
+            }
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
+
+
+        return pokemonTrainer;
+    }
+
+    @Override
+    public void saveData(Object object) throws IOException, PokedexException {
+
+        PokemonTrainer pokemonTrainer = (PokemonTrainer) object;
+
+        if (pokemonTrainer == null) {
+            throw new PokedexException("Couldn't save empty trainer !");
+        }
+
+        if (path == null) {
+            throw new PokedexException("Trainer path is not defined !");
+        }
+
+        String filename = this.getFileName(path, pokemonTrainer.getName());
+
+        this.persistData(filename, pokemonTrainer);
+    }
+
+    @Override
+    public Object loadData(String name) throws FileNotFoundException, PokedexException {
+
+        if (path == null) {
+            throw new PokedexException("Trainer path is not defined !");
+        }
+
+        PokemonTrainer pokemonTrainer = null;
+
+        String filename = this.getFileName(path, name);
+
+        try (Reader reader = new FileReader(filename)) {
+
+            Gson gson = new Gson();
+
+            pokemonTrainer = gson.fromJson(reader, PokemonTrainer.class);
+
+            System.out.println("trainer: " + pokemonTrainer.getName());
+
+            if (pokemonTrainer == null) {
+                throw new PokedexException("Error loading Trainer !");
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+
+        return pokemonTrainer;
+}
 }
